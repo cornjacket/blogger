415824f52af16c41b2c0d5202fdfd9e9fa1237d1
David Taylor <taylor.david.ray@gmail.com>
2014-12-12 20:49:25 -0800

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Blogger::Application.routes.draw do
+  root to: 'articles#index'
   # this statement tells Rails we are using RESTful convention
   resources :articles do
   	resources :comments
   end
+  resources :tags
   
-  root to: 'articles#index'
 end
